dd40d5e0f8f93e16e985158957ab457dacdc99d0
SheillyR <sheillyrolo@gmail.com>
2021-06-13 17:35:54 -0500

    1-Gradle project configuration

---
+++ src/test/java/com/globantproject/crudlibrary/CrudLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globantproject.crudlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
