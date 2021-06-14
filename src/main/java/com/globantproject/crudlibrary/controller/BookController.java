850d12eebc1481ef8ab496bf039e6fec51b00f50
SheillyR <sheillyrolo@gmail.com>
2021-06-14 18:09:15 -0500

    | WIP | controller package
    injection of BookService class in BookController

--- src/main/java/com/globantproject/crudlibrary/controller/BookController.java
+++ src/main/java/com/globantproject/crudlibrary/controller/BookController.java
@@ -1,41 +1,30 @@
 package com.globantproject.crudlibrary.controller;
 
 import com.globantproject.crudlibrary.model.Book;
-import com.globantproject.crudlibrary.model.ReservationInfo;
-import com.globantproject.crudlibrary.model.User;
+import com.globantproject.crudlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/book")
 public class BookController {
 
+    private final BookService bookService;
+
+    // Instance of BookService class in controller
+    // Add annotation @Service in BookService class to work
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping
     public List<Book> getBooks() {
-        return List.of(
-                new Book(
-                        1,
-                        "Title One",
-                        "Anonymous",
-                        LocalDate.of(2000, Month.JANUARY, 10),
-                        Boolean.TRUE,
-                        new ReservationInfo(
-                                new User(
-                                        "Maria",
-                                        "Lipa",
-                                        73589621,
-                                        "maria@gmail.com"
-                                ),
-                                LocalDate.of(2000, Month.JANUARY, 10),
-                                LocalDate.of(2000, Month.JANUARY, 10)
-                        )
-                )
-        );
+        return bookService.getBooks();
     }
 
 }
