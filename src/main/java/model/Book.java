1a7111e4d10764c29570abf6018a808edd3cf8ef
SheillyR <sheillyrolo@gmail.com>
2021-06-14 15:37:09 -0500

    | WIP |model
    change data type of identification, sort getters and setters

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -3,11 +3,12 @@ package model;
 import java.util.Date;
 
 public class Book {
-    private Integer id;
+    private int id;
     private String title;
     private String author;
     private Date editorialDate;
     private Boolean state;
+    private User user;
 
     public Book(Integer id, String title, String author, Date editorialDate, Boolean state) {
         this.id = id;
@@ -17,44 +18,51 @@ public class Book {
         this.state = state;
     }
 
-    public Integer getId() {
+    public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
-    public Date getEditorialDate() {
-        return editorialDate;
-    }
-
-    public Boolean getState() {
-        return state;
-    }
-
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
     }
 
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    public Date getEditorialDate() {
+        return editorialDate;
+    }
+
     public void setEditorialDate(Date editorialDate) {
         this.editorialDate = editorialDate;
     }
 
+    public Boolean getState() {
+        return state;
+    }
+
     public void setState(Boolean state) {
         this.state = state;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
