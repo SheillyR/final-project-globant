ee984875e0a8dd31e452ad8ef48acb6f89c0e8ba
SheillyR <sheillyrolo@gmail.com>
2021-06-14 16:54:44 -0500

    | WIP | main
    delete rest experiment in main method

--- src/main/java/com/globantproject/crudlibrary/CrudLibraryApplication.java
+++ src/main/java/com/globantproject/crudlibrary/CrudLibraryApplication.java
@@ -1,35 +1,14 @@
 package com.globantproject.crudlibrary;
 
-import model.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class CrudLibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudLibraryApplication.class, args);
 
-	}
-
-	@GetMapping
-	public List<Book> hello() {
-		return List.of(
-				new Book(
-						1,
-						"Title One",
-						"Anonymous",
-						LocalDate.of(2000, Month.JANUARY, 10),
-						Boolean.TRUE
-				)
-		);
 	}
 
 }
