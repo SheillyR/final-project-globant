b2d65217dbd9a8a13b2ed8599833ef481cf5c942
SheillyR <sheillyrolo@gmail.com>
2021-06-14 18:10:05 -0500

    | WIP | service package
    add method getBooks and annotation @Service

--- src/main/java/com/globantproject/crudlibrary/service/BookService.java
+++ src/main/java/com/globantproject/crudlibrary/service/BookService.java
@@ -1,2 +1,38 @@
-package com.globantproject.crudlibrary.service;public class BookService {
+package com.globantproject.crudlibrary.service;
+
+import com.globantproject.crudlibrary.model.Book;
+import com.globantproject.crudlibrary.model.ReservationInfo;
+import com.globantproject.crudlibrary.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    public List<Book> getBooks() {
+        return List.of(
+                new Book(
+                        1,
+                        "Title One",
+                        "Anonymous",
+                        LocalDate.of(2000, Month.JANUARY, 10),
+                        Boolean.TRUE,
+                        new ReservationInfo(
+                                new User(
+                                        "Maria",
+                                        "Lipa",
+                                        73589621,
+                                        "maria@gmail.com"
+                                ),
+                                LocalDate.of(2000, Month.JANUARY, 10),
+                                LocalDate.of(2000, Month.JANUARY, 10)
+                        )
+                )
+        );
+    }
 }
