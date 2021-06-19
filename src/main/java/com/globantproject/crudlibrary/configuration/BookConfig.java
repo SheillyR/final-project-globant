a12a884b1091fa08737de4ac72d616fba49be2b4
SheillyR <sheillyrolo@gmail.com>
2021-06-18 21:33:25 -0500

    add book objects by default to create elements into book table

--- src/main/java/com/globantproject/crudlibrary/configuration/BookConfig.java
+++ src/main/java/com/globantproject/crudlibrary/configuration/BookConfig.java
@@ -1,2 +1,35 @@
-package com.globantproject.crudlibrary.configuration;public class BookConfig {
+package com.globantproject.crudlibrary.configuration;
+
+import com.globantproject.crudlibrary.model.Book;
+import com.globantproject.crudlibrary.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class BookConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(BookRepository repository) {
+        return args -> {
+            Book book1 = new Book(
+                    "Title One",
+                    "Anonymous",
+                    2000,
+                    Boolean.TRUE
+            );
+            Book book2 = new Book(
+                    "Title Two",
+                    "Anonymous",
+                    1900,
+                    Boolean.TRUE
+            );
+
+            repository.saveAll(
+                    List.of(book1, book2)
+            );
+        };
+    }
 }
