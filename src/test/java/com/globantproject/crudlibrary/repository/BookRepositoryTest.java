da869c7fedf29f3ba22c6912810f545fe69f73f4
SheillyR <sheillyrolo@gmail.com>
2021-06-27 21:24:31 -0500

    add assertj and h2 dependencies to test

---
+++ src/test/java/com/globantproject/crudlibrary/repository/BookRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.globantproject.crudlibrary.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookRepositoryTest {
+
+    @Test
+    void findBookByAuthorAndTitle() {
+    }
+
+    @Test
+    void findBooksByState() {
+    }
+}
