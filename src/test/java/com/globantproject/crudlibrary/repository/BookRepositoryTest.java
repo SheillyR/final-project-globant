bdf9436873a359fb0260a85c32a16f940347972f
SheillyR <sheillyrolo@gmail.com>
2021-06-27 21:32:00 -0500

    | WIP |
    implement test to BookRepository

--- src/test/java/com/globantproject/crudlibrary/repository/BookRepositoryTest.java
+++ src/test/java/com/globantproject/crudlibrary/repository/BookRepositoryTest.java
@@ -1,15 +1,48 @@
 package com.globantproject.crudlibrary.repository;
 
+import com.globantproject.crudlibrary.model.Book;
+import com.globantproject.crudlibrary.model.Reservation;
+import com.globantproject.crudlibrary.model.State;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
 class BookRepositoryTest {
 
+    @Autowired
+    private BookRepository underTest;
+
     @Test
-    void findBookByAuthorAndTitle() {
+    void itShouldFindBookByAuthorAndTitle() {
+        // given
+        String title = "Title One";
+        String author = "Anonymous";
+        Reservation reservationInfoTest = null;
+
+        Book book = new Book(
+                title,
+                author,
+                2000,
+                State.AVAILABLE
+        );
+        book.setReservation(reservationInfoTest);
+        underTest.save(book);
+
+
+        // when
+        Optional<Book> expected = underTest.findBookByAuthorAndTitle(title, author);
+
+        // then
+        assertThat(book).usingRecursiveComparison().isEqualTo(expected);
     }
 
+    @Disabled
     @Test
     void findBooksByState() {
     }
